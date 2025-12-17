7c68d2b56a9adafa7dd3dbc6f256d04b7287e8f2
Asilbek-dev309 <asilbekjon0114@gmail.com>
2025-12-17 12:09:57 +0500

    done

--- file10.py
+++ file10.py
@@ -1,10 +1,8 @@
+a=open('data10.txt')
+b=a.readlines()
 def main(data:str):
-    """
-    The data is from the file. Find the each row length and return the largest row.
-    Args:
-        data: str
-    Returns:
-        int: return answer
-    """
-
-# Read data from file
+    d=[]
+    for i in b:
+        d+=[len(i)-1]
+    return max(d)
+print(main(str))
